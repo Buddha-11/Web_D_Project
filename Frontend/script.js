a0b58deea44f1090bea7209bae418b1ba8ef5c86
swarupn17 <swarupnarkhede999@gmail.com>
2024-03-15 12:35:06 +0530

    logo added

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,3 +3,31 @@ function togglePopup(){
 }
 window.onscroll = function() {scrollFunction()};
 
+function validate() {
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    var error = document.getElementById("errorMessage");
+
+    if (email === "" && pass === "") {
+        error.textContent = "Please fill out all fields !";
+    }
+
+    else if(email=="")
+    error.textContent = "Please enter your Email!";
+
+    else if(pass=="")
+    error.textContent = "Please enter your Password !";
+
+    else {
+        error.textContent = "";
+        alert("Sign In successfully!");
+    }
+}
+
+function scrollToDiv() {
+    var element = document.getElementByClass("gallery-container");
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+
+
