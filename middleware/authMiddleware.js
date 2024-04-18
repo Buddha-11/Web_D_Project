f198ad2357e5199b76f44a2335d5546fe6e412c1
Snehalgupta-07 <guptasnehal877@gmail.com>
2024-04-18 09:20:31 +0530

    UPDATE

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const requireAuth = (req, res, next) => {
     jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
       if (err) {
         console.log(err.message);
-        res.render('/login',{title: 'Login'});
+        res.render('/login');
       } else {
         console.log(decodedToken);
         next();
