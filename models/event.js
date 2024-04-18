4523b606249a027df54cc5251ad485bd1019c9d3
Arpit Anand <invoker.arpit@gmail.com>
2024-04-18 09:16:50 +0530

    update

--- models/event.js
+++ models/event.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const eventSchema = new Schema({
-    title: {
+    title:{
         type: String ,
         required: true
     },
